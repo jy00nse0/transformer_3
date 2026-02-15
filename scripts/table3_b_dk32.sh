b5f9edc6952d891745fbfb13c44bb091af568e78
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-02-15 21:34:54 +0900

    Update table3_b_dk32.sh

--- scripts/table3_b_dk32.sh
+++ scripts/table3_b_dk32.sh
@@ -18,7 +18,7 @@ python demo_wmt14_pretrained.py \
     --max_steps 100000
 
 # Inference & Evaluation
-python inference.py \
+python inference_batched.py \
     --checkpoint_path "checkpoints/table3_b_dk32/model_averaged_last5.pt" \
     --vocab_dir ${VOCAB_DIR} \
     --output_file "results/table3_b_dk32.txt"
